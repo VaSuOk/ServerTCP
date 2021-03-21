e696e8efd23c89506934351dde9adfb59e6c077b
VaSuOk <vasuok15vas@gmail.com>
2021-03-21 18:31:33 +0200

    the structure of user classes is created

--- Users/User.cs
+++ Users/User.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace Server.Users
 {
+    enum UserType
+    {
+        Unregistered,
+        Reseller,
+        Manufacture,
+        Moderator
+    }
     class User
     {
+        protected uint id;
+        protected UserType userType;
+        protected string PIB;
+
+        public User()
+        {
+            this.id = 0;
+            this.userType = UserType.Unregistered;
+            this.PIB = "";
+        }
     }
 }
